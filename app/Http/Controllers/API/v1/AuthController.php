e40e2d611f6b2b5c234b29b6bf975eb315addcf0
Solly <solobarine@gmail.com>
2023-06-22 01:58:46 +0100

    Change DB from MYSQL to Postgres

--- app/Http/Controllers/API/v1/AuthController.php
+++ app/Http/Controllers/API/v1/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         
         if (!$token) {
             return response()->json([
-                'message' => 'Unauthorized',
+                'message' => 'Invalid Email or Password',
             ], 401);
         }
 
