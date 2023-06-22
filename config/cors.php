3fe9c7122cbfa6e38f9f14859cfa649d184184d6
Solly <solobarine@gmail.com>
2023-06-23 00:41:22 +0100

    Add Frontend Production and Development routes for cors exemption

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'https://nova-solobarine.vercel.app/*', 'http://localhost:5173/*'],
 
     'allowed_methods' => ['*'],
 
